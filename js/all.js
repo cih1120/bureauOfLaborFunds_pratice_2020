788f32611a9422b975482ea3b4a931281e88fd90
cih1120 <44454902+cih1120@users.noreply.github.com>
2020-06-06 17:38:24 +0800

    0606

--- js/all.js
+++ js/all.js
@@ -3,7 +3,7 @@ var bottom_seal = document.querySelectorAll('.bottom_seal')
 var btn = document.querySelectorAll('.learn')
 
 var openFormBtn = document.getElementById('openForm')
-var peopleArray = JSON.parse(localStorage.getItem('seal'))||[];
+var peopleArray = JSON.parse(localStorage.getItem('value'))||[];
 var str = '';
 
 $('.openWin').click(function(event){
@@ -16,10 +16,9 @@ $('.people .winClose').click(function(event){
 	$(this).parent().parent().find('.window').fadeOut(100);
 })
 // 關閉人物視窗
-$('#openForm').click(function(){
+$('.js-open').click(function(){
 	console.log('hi');
 	event.preventDefault();
-
 	$('.displayNone').fadeIn(100);
 })
 // 打開填寫視窗
@@ -31,7 +30,31 @@ $('.sign_win .winClose').click(function(){
 
 
 let len = btn.length;
+reloadSeal();
+checkSeal();
 change();
+
+function reloadSeal(){
+	for(var i=0;i<len;i++){
+		if(peopleArray[i]==0){
+			content_seal[0].setAttribute('src','img/seal.png');
+	    	bottom_seal[0].src="img/seal.png";
+	    	btn[0].classList.add("defaultBtn");
+	    	btn[0].setAttribute('disabled','disabled');
+		}else if(peopleArray[i]==1){
+			content_seal[1].setAttribute('src','img/seal.png');
+	    	bottom_seal[1].src="img/seal.png";
+	    	btn[1].classList.add("defaultBtn");
+	    	btn[1].setAttribute('disabled','disabled');
+		}else if(peopleArray[i]==2){
+			content_seal[2].setAttribute('src','img/seal.png');
+	    	bottom_seal[2].src="img/seal.png";
+	    	btn[2].classList.add("defaultBtn");
+	    	btn[2].setAttribute('disabled','disabled');
+		}
+	}
+}
+
 function change(){
 	for(let i=0; i<len; i++) {
  	 btn[i].addEventListener('click',function(){
@@ -39,19 +62,27 @@ function change(){
     	bottom_seal[i].src="img/seal.png";
     	btn[i].classList.add("defaultBtn");
     	btn[i].setAttribute('disabled','disabled');
-    	peopleArray.push('1');
+    	peopleArray.push(i);
     	checkSeal();
+    	str = JSON.stringify(peopleArray);
+    	localStorage.setItem("value", str);
  	 })
 	}
 };
+//點選我已瞭解
 
 function checkSeal(){
 	if(peopleArray.length == 3){
-		openFormBtn.setAttribute('disabled','value');
+		openFormBtn.removeAttribute('disabled');
+		openFormBtn.classList.add("js-open");
 		document.getElementById("openForm").classList.remove("defaultBtn");
-		// $('.cardBtn').click(function(){
-		// event.preventDefault();
-		// $('.displayNone').fadeIn(100);
-		// })
 	}
+	$('.js-open').click(function(){
+	console.log('hi');
+	event.preventDefault();
+	$('.displayNone').fadeIn(100);})
+
+	$('#btn_send').click(function(){
+	alert('此活動已結束，感謝您的參加！');})
 }
+//三個按鈕集滿打開最下方填寫按鈕
